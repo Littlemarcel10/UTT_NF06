48e6ab9d472f42b2b4f553104c7e914d077b7df6
Gaudry Carlier <gaudry.carlier@utt.fr>
2022-10-03 15:30:58 +0200

    ex 1 cours

--- Semaine 4/Cours.c
+++ Semaine 4/Cours.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
+typedef union group{
+    short a;
+    char car;
+    char cac;
+} group;
 
-void main(){
-    
+int main(){
+    group gr;
+    gr.car= 'z'; gr.a=128;
+    printf("a=%d, car=%d and cac=%d\n", gr.a,gr.car,gr.cac);
+    printf("address of a %d\n", &(gr.a));
+    printf("address of car %d\n", &(gr.car));
+    return 0;
 }
